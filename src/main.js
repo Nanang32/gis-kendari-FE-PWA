131dc513989d5b908cb7629ecd2b282dc5b6621b
Luqman Fanani <luqman.fanani@gmail.com>
2022-09-09 17:52:03 +0800

    axios add

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from "./App.vue";
 import router from "./router";
 import globalComponents from "./global-components";
 import utils from "./utils";
+import axios from "axios";
+
 import "./assets/css/app.css";
 
 const app = createApp(App).use(router).use(createPinia());
 
 globalComponents(app);
 utils(app);
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL+'/api';
 
 app.mount("#app");
