001a256899c69bbebe7cbf8f8889d174c0c99692
Luqman Fanani <luqman.fanani@gmail.com>
2022-09-15 12:48:03 +0800

    halaman dengan 3 tab (tugas,fungsi | program | struktur) DUMMY PAGE

--- src/views/admin/profile/office_subuptdregion/Main.vue
+++ src/views/admin/profile/office_subuptdregion/Main.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="intro-y flex items-center mt-8">
-        <h2 class="text-lg font-medium mr-auto">Data list posting</h2>
+        <h2 class="text-lg font-medium mr-auto">Data Sekertariat dinas</h2>
     </div>
-    <div class="grid grid-cols-12 gap-6 mt-5 mb-20">
+    <div class="grid grid-cols-12 mt-5">
         <div class="intro-y col-span-12">
-            <button class="btn btn-primary shadow-md mr-2" @click="router.push({ name: 'admin-post-create'})">
-                Add new
+            <button class="btn btn-primary shadow-md mb-2" @click="router.push({ name: 'admin-secretariat-taskfunction'})">
+                Tambah data
             </button>
             <div class="intro-y box col-span-12 overflow-auto lg:overflow-visible">
+                <h3 class="text-sm text-center font-black p-2">Tugas dan fungsi</h3>
                 <table class="table table-report mt-2">
                     <thead>
                         <tr>
                             <th class="whitespace-nowrap">#</th>
                             <th class="whitespace-nowrap">Judul</th>
-                            <th class="whitespace-nowrap">Kategori</th>
-                            <th class="whitespace-nowrap">Tanggal</th>
+                            <th class="whitespace-nowrap">status</th>
                             <th class="text-center whitespace-nowrap">Action</th>
                         </tr>
                     </thead>
@@ -22,9 +22,107 @@
                         <tr>
                             <td>1</td>
                             <td>Lorem</td>
-                            <td>Video, news</td>
-                            <td>Published
-                                2022/08/30 at 7:18 am</td>
+                            <td>
+                                Aktif/ tidak
+                            </td>
+                            <td>
+                                <div class="flex justify-center items-center">
+                                    <button class="flex items-center mr-3">
+                                        <EditIcon class="w-4 h-4 mr-1" /> Edit
+                                    </button>
+                                    <a class="flex items-center text-danger">
+                                        <Trash2Icon class="w-4 h-4 mr-1" /> Delete
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- <Paginator
+        :page="page"
+        :perPage="perPage"
+        :lastPage="lastPage"
+        @goToPage="setPage"
+      />
+      <ModalConfirmDelete 
+        :isShowModal="showDeleteModal"
+        @confirm="onConfirmDelete"
+      /> -->
+        </div>
+    </div>
+    <div class="grid grid-cols-12 mt-5">
+        <div class="intro-y col-span-12">
+            <button class="btn btn-primary shadow-md mb-2" @click="router.push({ name: 'admin-post-create'})">
+                Add new
+            </button>
+            <div class="intro-y box col-span-12 overflow-auto lg:overflow-visible">
+                <h3 class="text-sm text-center font-black p-2">Program</h3>
+                <table class="table table-report mt-2">
+                    <thead>
+                        <tr>
+                            <th class="whitespace-nowrap">#</th>
+                            <th class="whitespace-nowrap">Kontent</th>
+                            <th class="whitespace-nowrap">status</th>
+                            <th class="text-center whitespace-nowrap">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Lorem</td>
+                            <td>
+                                Aktif/ tidak
+                            </td>
+                            <td>
+                                <div class="flex justify-center items-center">
+                                    <button class="flex items-center mr-3">
+                                        <EditIcon class="w-4 h-4 mr-1" /> Edit
+                                    </button>
+                                    <a class="flex items-center text-danger">
+                                        <Trash2Icon class="w-4 h-4 mr-1" /> Delete
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- <Paginator
+        :page="page"
+        :perPage="perPage"
+        :lastPage="lastPage"
+        @goToPage="setPage"
+      />
+      <ModalConfirmDelete 
+        :isShowModal="showDeleteModal"
+        @confirm="onConfirmDelete"
+      /> -->
+        </div>
+    </div>
+    <div class="grid grid-cols-12 mt-5">
+        <div class="intro-y col-span-12">
+            <button class="btn btn-primary shadow-md mb-2" @click="router.push({ name: 'admin-post-create'})">
+                Add new
+            </button>
+            <div class="intro-y box col-span-12 overflow-auto lg:overflow-visible">
+                <h3 class="text-sm text-center font-black p-2">Struktur</h3>
+                <table class="table table-report mt-2">
+                    <thead>
+                        <tr>
+                            <th class="whitespace-nowrap">#</th>
+                            <th class="whitespace-nowrap">File</th>
+                            <th class="whitespace-nowrap">status</th>
+                            <th class="text-center whitespace-nowrap">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Lorem</td>
+                            <td>
+                                Aktif/ tidak
+                            </td>
                             <td>
                                 <div class="flex justify-center items-center">
                                     <button class="flex items-center mr-3">
@@ -52,11 +150,11 @@
         </div>
     </div>
 </template>
-<!-- <script setup>
+<script setup>
 import Paginator from "@/components/paginator/Main.vue";
 import ModalConfirmDelete from "@/components/modal-confirm-delete/Main.vue";
 import sendRequest from '@libs/http.js'
-import { ref, watch , onMounted } from "vue";
+import { ref, watch, onMounted } from "vue";
 import { useRouter } from "vue-router";
 
 const facilities = ref([]);
@@ -67,43 +165,43 @@ const showDeleteModal = ref(false);
 const deleteId = ref(null);
 const router = useRouter();
 
-function setPage(newPage){
-  page.value = newPage
-} 
-
-function confirmDelete(id){
-  showDeleteModal.value = true
-  deleteId.value = id;
+function setPage(newPage) {
+    page.value = newPage
 }
 
-async function onConfirmDelete(){
-  const responseDelete = await sendRequest({
-    method: 'delete',
-    url: `/facilities/${deleteId.value}`,
-  });
-  showDeleteModal.value=false
-  await loadData(page.value);
+function confirmDelete(id) {
+    showDeleteModal.value = true
+    deleteId.value = id;
 }
 
-async function loadData(page=1){
-  const response = await sendRequest({
-      method: 'get',
-      url: '/facilities',
-      params: {
-        page: page
-      },
-  });
-  if ((response !== null) && (response.status === true)) {
-    facilities.value = response.data.facilities.data
-    lastPage.value = response.data.facilities.last_page
-  }
+async function onConfirmDelete() {
+    const responseDelete = await sendRequest({
+        method: 'delete',
+        url: `/facilities/${deleteId.value}`,
+    });
+    showDeleteModal.value = false
+    await loadData(page.value);
+}
+
+async function loadData(page = 1) {
+    const response = await sendRequest({
+        method: 'get',
+        url: '/facilities',
+        params: {
+            page: page
+        },
+    });
+    if ((response !== null) && (response.status === true)) {
+        facilities.value = response.data.facilities.data
+        lastPage.value = response.data.facilities.last_page
+    }
 }
 
 watch(page, async (newPage) => {
-  await loadData(newPage)
+    await loadData(newPage)
 })
 
-onMounted(async()=>{
-  await loadData()
+onMounted(async () => {
+    await loadData()
 });
-</script> -->
+</script>
--- src/views/admin/profile/office_subuptdregion/taskfunction/create.vue
+++ src/views/admin/profile/office_subuptdregion/taskfunction/create.vue
@@ -1,36 +1,7 @@
 <template>
   <div class="intro-y flex flex-col sm:flex-row items-center mt-8">
-    <h2 class="text-lg font-medium mr-auto">Add New Post</h2>
-    <div class="w-full sm:w-auto flex mt-4 sm:mt-0">
-      
-      <button
-        type="button"
-        class="btn box mr-2 flex items-center ml-auto sm:ml-0"
-      >
-        <EyeIcon class="w-4 h-4 mr-2" /> Preview
-      </button>
-      <Dropdown>
-        <DropdownToggle class="btn btn-primary shadow-md flex items-center">
-          Save <ChevronDownIcon class="w-4 h-4 ml-2" />
-        </DropdownToggle>
-        <DropdownMenu class="w-40">
-          <DropdownContent>
-            <DropdownItem>
-              <FileTextIcon class="w-4 h-4 mr-2" /> As New Post
-            </DropdownItem>
-            <DropdownItem>
-              <FileTextIcon class="w-4 h-4 mr-2" /> As Draft
-            </DropdownItem>
-            <DropdownItem>
-              <FileTextIcon class="w-4 h-4 mr-2" /> Export to PDF
-            </DropdownItem>
-            <DropdownItem>
-              <FileTextIcon class="w-4 h-4 mr-2" /> Export to Word
-            </DropdownItem>
-          </DropdownContent>
-        </DropdownMenu>
-      </Dropdown>
-    </div>
+    <h2 class="text-lg font-medium mr-auto">Register tugas dan fungsi</h2>
+    
   </div>
   <div class="grid grid-cols-12 gap-6 mt-5">
     <div class="intro-y col-span-12 lg:col-span-12">
