caf433227563913251bc6f0dba56fa21d96cc3f4
Luqman Fanani <luqman.fanani@gmail.com>
2022-09-19 11:01:23 +0800

    komponen tabel tugas dan fungsi

--- src/components/task-function-table/Main.vue
+++ src/components/task-function-table/Main.vue
@@ -2,71 +2,53 @@
     <div class="intro-y flex items-center mt-8">
         <h2 class="text-lg font-medium mr-auto">Data Sekertariat dinas</h2>
     </div>
-    <TasknFunctionTable
-        addRouteName="admin-secretariat-taskfunction"
-        editRouteName="admin-secretariat-taskfunction-edit"
-        path="sekretariat-dinas"
-    />
-    <WorkingProgramTable
-        addRouteName="admin-secretariat-programme"
-        editRouteName="admin-secretariat-programme-edit"
-        path="sekretariat-dinas"
-    />
     <div class="grid grid-cols-12 mt-5">
         <div class="intro-y col-span-12">
-            <button class="btn btn-primary shadow-md mb-2" @click="router.push({ name: 'admin-secretariat-structure'})">
-                Add new
+            <button class="btn btn-primary shadow-md mb-2" @click="router.push({ name: 'admin-secretariat-taskfunction'})">
+                Tambah data
             </button>
             <div class="intro-y box col-span-12 overflow-auto lg:overflow-visible">
-                <h3 class="text-sm text-center font-black p-2">Struktur</h3>
+                <h3 class="text-sm text-center font-black p-2">Tugas dan fungsi</h3>
                 <table class="table table-report mt-2">
                     <thead>
                         <tr>
                             <th class="whitespace-nowrap">#</th>
-                            <th class="whitespace-nowrap">File</th>
+                            <th class="whitespace-nowrap">Judul</th>
                             <th class="whitespace-nowrap">status</th>
                             <th class="text-center whitespace-nowrap">Action</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody v-for="(taskFunction, index) in taskFunctions" :key="index">
                         <tr>
-                            <td>1</td>
-                            <td>Lorem</td>
+                            <td>{{ index + 1 }}</td>
+                            <td>{{ taskFunction.title}}</td>
+
                             <td>
                                 Aktif/ tidak
                             </td>
                             <td>
-                                <div class="flex justify-center items-center">
-                                    <button class="flex items-center mr-3">
+                                 <div class="flex justify-center items-center">
+                                    <button class="flex items-center mr-3"
+                                        @click="router.push({name: editRouteName, params: { id: taskFunction.id }})">
                                         <EditIcon class="w-4 h-4 mr-1" /> Edit
                                     </button>
-                                    <a class="flex items-center text-danger">
+                                    <button class="flex items-center text-danger"
+                                        @click="confirmDelete(taskFunction.id)">
                                         <Trash2Icon class="w-4 h-4 mr-1" /> Delete
-                                    </a>
+                                    </button>
                                 </div>
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-            <Paginator
-        :page="page"
-        :perPage="perPage"
-        :lastPage="lastPage"
-        @goToPage="setPage"
-      />
-      <ModalConfirmDelete 
-        :isShowModal="showDeleteModal"
-        @confirm="onConfirmDelete"
-      />
+            <Paginator :page="page" :perPage="perPage" :lastPage="lastPage" @goToPage="setPage" />
+            <ModalConfirmDelete :isShowModal="showDeleteModal" @confirm="onConfirmDelete" />
         </div>
     </div>
 </template>
 <script setup>
 import Paginator from "@/components/paginator/Main.vue";
-import WorkingProgramTable from "@/components/working-program-table/Main.vue";
-import TasknFunctionTable from "@/components/task-function-table/Main.vue";
-
 import ModalConfirmDelete from "@/components/modal-confirm-delete/Main.vue";
 import sendRequest from '@libs/http.js'
 import { ref, watch, onMounted } from "vue";
@@ -80,6 +62,18 @@ const showDeleteModal = ref(false);
 const deleteId = ref(null);
 const router = useRouter();
 
+const props = defineProps({
+    addRouteName: {
+        type: String,
+    },
+    editRouteName: {
+        type: String,
+    },
+    path: {
+        type: String,
+    }
+})
+
 function setPage(newPage) {
     page.value = newPage
 }
@@ -92,7 +86,7 @@ function confirmDelete(id) {
 async function onConfirmDelete() {
     const responseDelete = await sendRequest({
         method: 'delete',
-        url: `taskFunction/sekretariat-dinas/${deleteId.value}`,
+        url: `taskFunction/${props.path}/${deleteId.value}`,
     });
     showDeleteModal.value = false
     await loadData(page.value);
@@ -101,7 +95,7 @@ async function onConfirmDelete() {
 async function loadData(page = 1) {
     const response = await sendRequest({
         method: 'get',
-        url: 'taskFunction/sekretariat-dinas/',
+        url: `taskFunction/${props.path}/`,
         params: {
             page: page
         },
