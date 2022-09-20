d910e8500c341b7481f0b5a44f8d6c10bb0c06c4
Luqman Fanani <luqman.fanani@gmail.com>
2022-09-20 11:52:28 +0800

    set props pada tabel

--- src/components/task-function-table/Main.vue
+++ src/components/task-function-table/Main.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="grid grid-cols-12 mt-5">
         <div class="intro-y col-span-12">
-            <button class="btn btn-primary shadow-md mb-2" @click="router.push({ name: 'admin-secretariat-taskfunction'})">
+           <button class="btn btn-primary shadow-md mb-2"
+                @click="router.push({ name: addRouteName })">
                 Tambah data
             </button>
             <div class="intro-y box col-span-12 overflow-auto lg:overflow-visible">
@@ -19,18 +20,15 @@
                         <tr>
                             <td>{{ index + 1 }}</td>
                             <td>{{ taskFunction.title}}</td>
-
                             <td>
                                 Aktif/ tidak
                             </td>
                             <td>
-                                 <div class="flex justify-center items-center">
-                                    <button class="flex items-center mr-3"
-                                        @click="router.push({name: editRouteName, params: { id: taskFunction.id }})">
+                                <div class="flex justify-center items-center">
+                                    <button class="flex items-center mr-3" @click="router.push({name: editRouteName, params: { id: taskFunction.id }})">
                                         <EditIcon class="w-4 h-4 mr-1" /> Edit
                                     </button>
-                                    <button class="flex items-center text-danger"
-                                        @click="confirmDelete(taskFunction.id)">
+                                    <button class="flex items-center text-danger" @click="confirmDelete(taskFunction.id)">
                                         <Trash2Icon class="w-4 h-4 mr-1" /> Delete
                                     </button>
                                 </div>
