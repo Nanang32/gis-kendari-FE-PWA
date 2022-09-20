9d20a14a825f00b7ba8d2c06e9e52d3af7ca41c8
Luqman Fanani <luqman.fanani@gmail.com>
2022-09-20 13:13:53 +0800

    master data bina marga

--- src/views/admin/profile/office_subbinamarga/structure/edit.vue
+++ src/views/admin/profile/office_subbinamarga/structure/edit.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="intro-y flex items-center mt-8">
-    <h2 class="text-lg font-medium mr-auto">Form Layout</h2>
+    <h2 class="text-lg font-medium mr-auto">Form Pembaruan struktur</h2>
   </div>
   <div class="grid grid-cols-12 gap-6 mt-5">
     <div class="intro-y col-span-12 lg:col-span-6">
       <Form
         :loading="loading"
-        :facility="facility"
+        :formData="formData"
         @submit="onSubmit"
+        @fileChange="onFileChange"
       ></Form>
     </div>
   </div>
@@ -15,38 +16,47 @@
 
 <script setup>
 import sendRequest from '@libs/http.js'
-import Form from "./components/Form.vue";
+import Form from "@/components/form-image/Main.vue";
 import { useRoute, useRouter } from "vue-router";
 import { ref, reactive, onMounted } from "vue";
 const route = useRoute();
 const router = useRouter();
 const loading = ref(false);
-let facility = reactive({
-    name: null
+let formData = reactive({
+    title: null,
 })
+const image = ref('');
+const onFileChange = (e) => {
+  image.value = e.target.files[0];
+};
 
 onMounted(async () => {
   loading.value = true;
   const response = await sendRequest({
       method: 'GET',
-      url: `/facilities/${route.params.id}`
+      url: `/organizationDiagram/bina-marga/${route.params.id}`
   });
   if ((response !== null) && (response.status === true)) {
-    Object.assign(facility, response.data.facility);
+    Object.assign(formData, response.data.organizationDiagram);
   }
   loading.value = false;
 })
 
 async function onSubmit(){
   loading.value = true;
+  const formFileData = new FormData();
+  formFileData.append('title', formData.title)
+  formFileData.append('file', image.value)
+  formFileData.append('_method', 'PUT')
   const response = await sendRequest({
-      method: 'PUT',
-      url: `/facilities/${route.params.id}`,
-      data: facility
+      method: 'POST',
+      url: `/organizationDiagram/bina-marga/${route.params.id}`,
+      data: formFileData
   });
   loading.value = false;
   if ((response !== null) && (response.status === true)) {
-    router.push({name: 'admin-facility'});
+    router.push({name: 'admin-binamarga'});
   }
 }
 </script>
+
--- src/views/admin/profile/office_subbinamarga/taskfunction/edit.vue
+++ src/views/admin/profile/office_subbinamarga/taskfunction/edit.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="intro-y flex items-center mt-8">
-    <h2 class="text-lg font-medium mr-auto">Form Layout</h2>
+    <h2 class="text-lg font-medium mr-auto">Form pembaruan tugas dan fungsi</h2>
   </div>
   <div class="grid grid-cols-12 gap-6 mt-5">
     <div class="intro-y col-span-12 lg:col-span-6">
       <Form
         :loading="loading"
-        :facility="facility"
+        :formData="formData"
         @submit="onSubmit"
       ></Form>
     </div>
@@ -15,24 +15,25 @@
 
 <script setup>
 import sendRequest from '@libs/http.js'
-import Form from "./components/Form.vue";
+import Form from "@/components/form-editor/Main.vue";
 import { useRoute, useRouter } from "vue-router";
 import { ref, reactive, onMounted } from "vue";
 const route = useRoute();
 const router = useRouter();
 const loading = ref(false);
-let facility = reactive({
-    name: null
+let formData = reactive({
+    title: null,
+    content: ''
 })
 
 onMounted(async () => {
   loading.value = true;
   const response = await sendRequest({
       method: 'GET',
-      url: `/facilities/${route.params.id}`
+      url: `/taskFunction/bina-marga/${route.params.id}`
   });
   if ((response !== null) && (response.status === true)) {
-    Object.assign(facility, response.data.facility);
+    Object.assign(formData, response.data.taskFunction);
   }
   loading.value = false;
 })
@@ -41,12 +42,12 @@ async function onSubmit(){
   loading.value = true;
   const response = await sendRequest({
       method: 'PUT',
-      url: `/facilities/${route.params.id}`,
-      data: facility
+      url: `/taskFunction/bina-marga/${route.params.id}`,
+      data: formData
   });
   loading.value = false;
   if ((response !== null) && (response.status === true)) {
-    router.push({name: 'admin-facility'});
+    router.push({name: 'admin-binamarga'});
   }
 }
 </script>
