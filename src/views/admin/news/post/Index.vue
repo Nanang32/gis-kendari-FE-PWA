d1df6f1b9192d33bf13109c2f9f25911ac207e8b
Handy Handoko <468608+handyhandoko@users.noreply.github.com>
2022-09-21 19:02:27 +0800

    fix load data on post

--- src/views/admin/news/post/Index.vue
+++ src/views/admin/news/post/Index.vue
@@ -1,65 +1,65 @@
 <template>
-    <div class="intro-y flex items-center mt-8">
-        <h2 class="text-lg font-medium mr-auto">Data list posting</h2>
-    </div>
-    <div class="grid grid-cols-12 gap-6 mt-5 mb-20">
-        <div class="intro-y col-span-12">
-            <button class="btn btn-primary shadow-md mr-2" @click="router.push({ name: 'admin-post-create'})">
-                Add new
-            </button>
-            <div class="intro-y box col-span-12 overflow-auto lg:overflow-visible">
-                <table class="table table-report mt-2">
-                    <thead>
-                        <tr>
-                            <th class="whitespace-nowrap">#</th>
-                            <th class="whitespace-nowrap">Judul</th>
-                            <th class="whitespace-nowrap">Kategori</th>
-                            <th class="whitespace-nowrap">Tanggal</th>
-                            <th class="text-center whitespace-nowrap">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Lorem</td>
-                            <td>Video, news</td>
-                            <td>Published
-                                2022/08/30 at 7:18 am</td>
-                            <td>
-                                <div class="flex justify-center items-center">
-                                    <button class="flex items-center mr-3">
-                                        <EditIcon class="w-4 h-4 mr-1" /> Edit
-                                    </button>
-                                    <a class="flex items-center text-danger">
-                                        <Trash2Icon class="w-4 h-4 mr-1" /> Delete
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <!-- <Paginator
-        :page="page"
-        :perPage="perPage"
-        :lastPage="lastPage"
-        @goToPage="setPage"
-      />
-      <ModalConfirmDelete 
-        :isShowModal="showDeleteModal"
-        @confirm="onConfirmDelete"
-      /> -->
-        </div>
-    </div>
+  <div class="intro-y flex items-center mt-8">
+      <h2 class="text-lg font-medium mr-auto">Data list posting</h2>
+  </div>
+  <div class="grid grid-cols-12 gap-6 mt-5 mb-20">
+      <div class="intro-y col-span-12">
+          <button class="btn btn-primary shadow-md mr-2" @click="router.push({ name: 'admin-post-create'})">
+              Add new
+          </button>
+          <div class="intro-y box col-span-12 overflow-auto lg:overflow-visible">
+              <table class="table table-report mt-2">
+                  <thead>
+                      <tr>
+                          <th class="whitespace-nowrap">#</th>
+                          <th class="whitespace-nowrap">Judul</th>
+                          <th class="whitespace-nowrap">Kategori</th>
+                          <th class="whitespace-nowrap">Tanggal</th>
+                          <th class="text-center whitespace-nowrap">Action</th>
+                      </tr>
+                  </thead>
+                  <tbody v-for="(post, index) in posts" :key="index">
+                      <tr>
+                          <td> {{ index +1 }}</td>
+                          <td> {{ post.title }} </td>
+                          <td>Video, news</td>
+                          <td>Published
+                              2022/08/30 at 7:18 am</td>
+                          <td>
+                              <div class="flex justify-center items-center">
+                                  <button class="flex items-center mr-3">
+                                      <EditIcon class="w-4 h-4 mr-1" /> Edit
+                                  </button>
+                                  <a class="flex items-center text-danger">
+                                      <Trash2Icon class="w-4 h-4 mr-1" /> Delete
+                                  </a>
+                              </div>
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
+          <Paginator
+            :page="page"
+            :perPage="perPage"
+            :lastPage="lastPage"
+            @goToPage="setPage"
+          />
+          <ModalConfirmDelete 
+            :isShowModal="showDeleteModal"
+            @confirm="onConfirmDelete"
+          />
+      </div>
+  </div>
 </template>
-<!-- <script setup>
+<script setup>
 import Paginator from "@/components/paginator/Main.vue";
 import ModalConfirmDelete from "@/components/modal-confirm-delete/Main.vue";
 import sendRequest from '@libs/http.js'
 import { ref, watch , onMounted } from "vue";
 import { useRouter } from "vue-router";
 
-const facilities = ref([]);
+const posts = ref([]);
 const page = ref(1);
 const perPage = ref(10);
 const lastPage = ref(1);
@@ -79,7 +79,7 @@ function confirmDelete(id){
 async function onConfirmDelete(){
   const responseDelete = await sendRequest({
     method: 'delete',
-    url: `/facilities/${deleteId.value}`,
+    url: `/posts/${deleteId.value}`,
   });
   showDeleteModal.value=false
   await loadData(page.value);
@@ -88,14 +88,14 @@ async function onConfirmDelete(){
 async function loadData(page=1){
   const response = await sendRequest({
       method: 'get',
-      url: '/facilities',
+      url: '/posts',
       params: {
         page: page
       },
   });
   if ((response !== null) && (response.status === true)) {
-    facilities.value = response.data.facilities.data
-    lastPage.value = response.data.facilities.last_page
+    posts.value = response.data.posts.data
+    lastPage.value = response.data.posts.last_page
   }
 }
 
@@ -106,4 +106,4 @@ watch(page, async (newPage) => {
 onMounted(async()=>{
   await loadData()
 });
-</script> -->
+</script>
