53712e432f96f8ee69af930e98aa59b0eda97304
Luqman Fanani <luqman.fanani@gmail.com>
2022-10-10 15:59:18 +0800

    halmaan aliran sungai

--- src/views/admin/gis/watershed/components/Form.vue
+++ src/views/admin/gis/watershed/components/Form.vue
@@ -1,45 +1,270 @@
 <template>
-    <div class="intro-y box p-5">
-        <div>
-            <label class="form-label">Category name</label>
-            <input
-                type="text"
-                class="form-control w-full"
-                placeholder="Input text"
-                v-model="category.name"
-                :disabled="loading"
-            />
+    <div class="grid grid-cols-12 gap-6">
+        <div class="box col-span-12 lg:col-span-12">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-red-800">
+                <h2 class="text-base text-white  uppercase">form isian data dasar infrastruktur</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label for="horizontal-form-1" class="form-label sm:w-30 capitalize">Unit eselon i</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-2" class="form-label sm:w-30 capitalize">unit eselon II.A</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-2" class="form-label sm:w-30 capitalize">unit eselon II.B/III.A</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
+            <div class="p-5 border">
+                <div class="form-inline">
+                    <label for="horizontal-form-1" class="form-label sm:w-30 capitalize">kelompok data dasar</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-2" class="form-label sm:w-30 capitalize">nama data dasar</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-2" class="form-label sm:w-30 capitalize">tahun data</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-2" class="form-label sm:w-30 capitalize">kondisi</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
         </div>
-        <div class="text-right mt-5">
-            <button type="button" class="btn btn-outline-secondary w-24 mr-1">
-            Cancel
-            </button>
-            <button
-                type="button"
-                class="btn btn-primary w-24"
-                @click="submit"
-                :disabled="loading"
-            >
-                <LoadingIcon icon="tail-spin" class="w-8 h-8" v-if="loading" />
-                Save
-            </button>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white mr-auto uppercase">I.informasi wilayah sungai</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-20 capitalize">nama WS</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20">Status</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Nasional</option>
+                        <option>Provinsi</option>
+                        <option>Kabupaten/Kota</option>
+                        <option>Non Status</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">ii.Kodefikasi</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-30 capitalize">kode bidang pekerjaan umum</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">kode data dasar jenis infrastruktur</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">kode infrastruktur (sesuai ketentuan ditjen SDA)</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white mr-auto uppercase">Iii.lokasi</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-20">propinsi</label>
+                    <input type="text" class="form-control" placeholder="Ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">kota/kab.</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">kecamatan</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">kel./Desa</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">lokasi</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">iv.manfaat</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-30">irigasi (ha)</label>
+                    <input type="text" class="form-control" placeholder="Ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">DMI (m<sup>3</sup>/detik)</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">PLTA (MWH)</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">Ternak (ekor)</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">lain-lain</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-12">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">v.data teknis</h2>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-30">luas (km<sup>2</sup>)</label>
+                    <input type="text" class="form-control" placeholder="Ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">tanggul</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak sedang</option>
+                        <option>Rusak berat</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">parapet</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak sedang</option>
+                        <option>Rusak berat</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">revetment</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak sedang</option>
+                        <option>Rusak berat</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">ambang dasar / ground sill</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak sedang</option>
+                        <option>Rusak berat</option>
+                    </select>
+                </div>
+                
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="p-5">
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">krib</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak sedang</option>
+                        <option>Rusak berat</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">bangunan drainase/ sluice</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak sedang</option>
+                        <option>Rusak berat</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">stasiun pompa</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak sedang</option>
+                        <option>Rusak berat</option>
+                    </select>
+                </div>
+               <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">sabo DAM / check DAM</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak sedang</option>
+                        <option>Rusak berat</option>
+                    </select>
+                </div>
+                
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">keterangan</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">vi.koordinat (decimal degree)</h2>
+            </div>
+            <div class="p-5 text-center">
+                <h1 class="font-medium text-5xl text-yellow-800"> Partial page</h1>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">vii.photo dan video</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-30">photo</label>
+                    <input type="file" class="form-control" />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">video</label>
+                    <input type="file" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
         </div>
     </div>
+    <div class=" mt-5">
+        <button type="button" class="btn btn-outline-secondary w-24 mr-1">
+            Batal
+        </button>
+        <button type="button" class="btn btn-primary w-24">
+            <!-- <LoadingIcon icon="tail-spin" class="w-8 h-8" v-if="loading" /> -->
+            Simpan
+        </button>
+    </div>
 </template>
-<script setup>
-    import { ref } from "vue";
-    defineProps({
-        loading: {
-            type: Boolean,
-            default: false
-        },
-        category: {
-            type: Object,
-            default: {name: null}
-        }
-    })
-    const emit = defineEmits(['submit'])
-    function submit() {
-        emit('submit');
-    }
-</script>
