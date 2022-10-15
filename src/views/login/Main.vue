baedae436590d692ef6a8bf5eb6ef00749b3e97f
Luqman Fanani <luqman.fanani@gmail.com>
2022-10-16 00:34:10 +0800

    untuk keperluan persentase

--- src/views/login/Main.vue
+++ src/views/login/Main.vue
@@ -10,17 +10,17 @@
             <img
               alt="Midone Tailwind HTML Admin Template"
               class="-intro-x w-1/2 -mt-16"
-              src="@/assets/images/logo.svg"
+              src="@/assets/images/logosultra.png"
             />
             <div
-              class="-intro-x text-white font-medium text-4xl leading-tight mt-10"
+              class="-intro-x text-white pl-20 font-medium text-2xl leading-tight mt-10"
             >
-              Provinsi sulawesi tenggara
+              SISDJATAN
             </div>
             <div
-              class="-intro-x text-white uppercase leading-tight mt-10"
+              class="-intro-x text-white pl-14 uppercase leading-tight mt-10"
             >
-              SISDJATAN (Sistem Informasi SDA Jalan Jembatan)
+              Provinsi sulawesi tenggara
             </div>
             <div
               class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400 capitalize"
@@ -62,7 +62,7 @@
                 class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top"
                 @click="onLogin"
               >
-                Login
+                Masuk
               </button>
             </div>
           </div>
