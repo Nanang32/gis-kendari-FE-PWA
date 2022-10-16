5879c07119e1e70097fc204f267e8805bc52a738
Handy Handoko <468608+handyhandoko@users.noreply.github.com>
2022-10-17 06:09:41 +0800

    fix bug task function

--- src/views/public/Profile/tugasfungsi.vue
+++ src/views/public/Profile/tugasfungsi.vue
@@ -6,25 +6,24 @@
                 <img alt="Enigma Tailwind HTML Admin Template" class="object-cover" src="@/assets/images/tugasdanfungsi.png" />
             </div>
         </div>
-        <div class="intro-y text-justify leading-relaxed">
-            <h1 class="font-medium text-xl sm:text-2xl  text-center mb-5">Tugas pokok</h1>
-            <p class="mb-5">
-                Dinas Sumber Daya Air dan Bina Marga Provinsi Sulawesi Tenggara merupakan Organisasi Perangkat Daerah (OPD) yang dibentuk berdasarkan Peraturan Daerah Provinsi Sulawesi Tenggara Nomor 13 Tahun 2016 tentang Pembentukan dan Susunan Perangkat Daerah. SKPD Dinas Sumber Daya Air dan Bina Marga Provinsi Sulawesi Tenggara yang merupakan perubahan bentuk dari Dinas Pekerjaan Umum Provinsi Sulawesi Tenggara ini secara struktural bertanggung jawab kepada Gubernur melalui Sekretaris Daerah Provinsi Sulawesi Tenggara. <br>
-                Sesuai Peraturan Gubernur Sulawesi Tenggara Nomor 58 Tahun 2016 tentang Kedudukan, Struktur Organisasi, Tugas dan Fungsi serta Tata Kerja Dinas Sumber Daya Air dan Bina Marga Provinsi Sulawesi Tenggara memiliki tugas membantu Gubernur melaksanakan urusan pemerintahan yang menjadi kewenangan daerah dan tugas pembantuan di bidang pekerjaan umum dan penataan ruang sub bidang sumber daya air dan sub bidang bina marga. Dalam melaksanakan tugas tersebut, Dinas Sumber Daya Air dan Bina Marga Provinsi Sulawesi Tenggara menyelenggarakan fungsi sebagai berikut :
-            </p>
-            <h1 class="font-medium text-xl sm:text-2xl  text-center mb-5">Fungsi</h1>
-            <p class="mb-5">1. Penyusunan konsep kebijakan pengelolaan sumber daya air dan rencana pengelolaan sumber daya air pada wilayah sungai; <br>
-                2. Penyusunan pola pengelolaan sumber daya air dan rencana pengelolaan sumber daya air pada wilayah sungai; <br>
-                3. Penyusunan program pengelolaan sumber daya air dan rencana kegiatan pengelolaan sumber daya air pada wilayah sungai; <br>
-                4. Pelaksanaan pengelolaan sumber daya air dan rencana pengelolaan sumber daya air pada wilayah sungai; <br>
-                5. Pemantauan dan evaluasi penyelenggaraan / penerapan pola pengelolaan sumber daya air dan rencana pengelolaan sumber daya air; <br>
-                6. Pelaksanaan koordinasi pemrograman dan perencanaan teknik jalan, konektivitas sistem jaringan jalan dengan sistem moda transportasi bersama instansi terkait; <br>
-                7. Pelaksanaan perencanaan teknik jalan, jembatan, penerangan jalan umum, peralatan, dan pengujian; <br>
-                8. Pelaksanaan pembangunan dan preservasi jalan dan jembatan, dan penerangan jalan umum; <br>
-                9. Pelaksanaan evaluasi dan penetapan laik fungsi, audit keselamatan jalan dan jembatan serta leger jalan; <br>
-                10. Pelaksanaan administrasi Dinas; <br>
-                11. Pelaksanaan fungsi lain yang diberikan oleh Gubernur sesuai dengan tugas dan fungsinya.
-            </p>
+        <div class="intro-y text-justify leading-relaxed" v-html="content">
         </div>
     </div>
 </template>
+
+<script setup>
+  import sendRequest from '@libs/http.js'
+  import { ref, onMounted } from "vue";
+  const loading = ref(false);
+  const content = ref('');
+
+  onMounted(async () => {
+    const response = await sendRequest({
+        method: 'get',
+        url: '/public/departmentTasks',
+    });
+    if ((response !== null) && (response.status === true)) {
+        content.value = response.data.departmentTask.content
+    }
+  });
+</script>
