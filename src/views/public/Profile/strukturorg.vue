14043de8c0e3e616c8c5faceee2b3aaacc7a7048
Handy Handoko <468608+handyhandoko@users.noreply.github.com>
2022-10-17 07:03:22 +0800

    show image

--- src/views/public/Profile/strukturorg.vue
+++ src/views/public/Profile/strukturorg.vue
@@ -1,7 +1,29 @@
 <template>
-    <!-- BEGIN: Page Layout -->
-    <div class="box mt-32">
-        <img alt="Midone Tailwind HTML Admin Template" src="@/assets/images/picture1.png" />
+    <div class="intro-y news  p-5 box m-3">
+       <div class="intro-y mt-3 mb-3">
+              <div class="news__preview image-fit">
+                  <img alt="Enigma Tailwind HTML Admin Template" class="object-cover" src="@/assets/images/visimisi.png" />
+              </div>
+          </div>
+      <div class="intro-y text-justify leading-relaxed">
+        <img :src="image" />
+      </div>
     </div>
-    <!-- END: Page Layout< -->
-</template>
+  </template>
+  
+  <script setup>
+    import sendRequest from '@libs/http.js'
+    import { ref, onMounted } from "vue";
+    const loading = ref(false);
+    const image = ref('');
+  
+    onMounted(async () => {
+      const response = await sendRequest({
+          method: 'get',
+          url: '/public/departmentOrganizations',
+      });
+      if ((response !== null) && (response.status === true)) {
+          image.value = response.data.departmentOrganization.file_url
+      }
+    });
+  </script>
