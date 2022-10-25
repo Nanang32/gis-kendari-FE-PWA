23447ef002ad888e9bced142134e56e23a73fb72
Luqman Fanani <luqman.fanani@gmail.com>
2022-10-25 08:33:17 +0800

    form jembatan

--- src/views/admin/gis/bridge/components/Form.vue
+++ src/views/admin/gis/bridge/components/Form.vue
@@ -1,45 +1,336 @@
 <template>
-    <div class="intro-y box p-5">
-        <div>
-            <label class="form-label">Category name</label>
-            <input
-                type="text"
-                class="form-control w-full"
-                placeholder="Input text"
-                v-model="category.name"
-                :disabled="loading"
-            />
+    <div class="grid grid-cols-12 gap-6">
+        <div class="box col-span-12 lg:col-span-12">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-red-800">
+                <h2 class="text-base text-white  uppercase">form isian data dasar infrastruktur</h2>
+            </div>
+            <div class="p-5 border">
+                <div class="form-inline">
+                    <label for="horizontal-form-1" class="form-label sm:w-30 capitalize">kelompok data dasar infrastruktur</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-2" class="form-label sm:w-30 capitalize">nama data dasar infrastruktur</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-2" class="form-label sm:w-30 capitalize">tahun data</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-2" class="form-label sm:w-30 capitalize">status</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Nasional tol</option>
+                        <option>Nasional non tol</option>
+                        <option>Provinsi</option>
+                        <option>Kabupaten/Kota</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-2" class="form-label sm:w-30 capitalize">Fungsi</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Arteri primer</option>
+                        <option>Arteri sekunder</option>
+                        <option>Kolektor primer</option>
+                        <option>Kolektor sekunder</option>
+                        <option>Lokal</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-1" class="form-label sm:w-30 capitalize">Mendukung</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Strategis</option>
+                        <option>Non strategis</option>
+                        <option>Kolektor primer</option>
+                        <option>Kolektor sekunder</option>
+                        <option>Lokal</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label for="horizontal-form-1" class="form-label sm:w-30 capitalize">uraian dukungan</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
         </div>
-        <div class="text-right mt-5">
-            <button type="button" class="btn btn-outline-secondary w-24 mr-1">
-            Cancel
-            </button>
-            <button
-                type="button"
-                class="btn btn-primary w-24"
-                @click="submit"
-                :disabled="loading"
-            >
-                <LoadingIcon icon="tail-spin" class="w-8 h-8" v-if="loading" />
-                Save
-            </button>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">i.Kodefikasi kelompok data</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-30 capitalize">kode kementrian</label>
+                    <input type="text" class="form-control" v-model="road.kode_bidang_pekerjaan_umum" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">kode unit organisasi</label>
+                    <input type="text" class="form-control" v-model="road.kode_data_dasar_jenis_infrastruktur" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">kode data dasar infrastruktur</label>
+                    <input type="text" class="form-control"  placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">kode jembatan</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">kode infrastruktur</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">Nomor jembatan</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white mr-auto uppercase">Ii.lokasi</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-20">provinsi</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">kota/kab.</label>
+                    <input type="text" class="form-control"  placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">kecamatan</label>
+                    <input type="text" class="form-control"  placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">kelurahan/Desa</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">Dari (Kota)</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">KM</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white mr-auto uppercase">Iii.kondisi (Dalam KM)</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-20 capitalize">Kondisi umum jembatan</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Sedang</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak berat</option>
+                        <option>Kritis</option>
+                        <option>Runtuh</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">Kondisi bangunan atas</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Sedang</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak berat</option>
+                        <option>Kritis</option>
+                        <option>Runtuh</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">kondisi lantai</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Sedang</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak berat</option>
+                        <option>Kritis</option>
+                        <option>Runtuh</option>
+                    </select>>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">kondisi bangunan bawah</label>
+                    <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Sedang</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak berat</option>
+                        <option>Kritis</option>
+                        <option>Runtuh</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">kondisi daerah aliran sungai</label>
+                  <select class="form-select mt-2 sm:mr-2" aria-label="Default select example">
+                        <option>--Pilih salah satu--</option>
+                        <option>Baik</option>
+                        <option>Sedang</option>
+                        <option>Rusak ringan</option>
+                        <option>Rusak berat</option>
+                        <option>Kritis</option>
+                        <option>Runtuh</option>
+                    </select>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-20 capitalize">tanggal inspeksi detail</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">iv.data teknis</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">Panjang (m)</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">Lebar (m)</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">tipe bangunan atas</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">lebar trotoar (m)</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">jumlah bentang</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">clearennce (Tinggi ruang bebas) (m)</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                 <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">tahun pembangunan</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">tahun penanganan terakhir</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">Jenis penanganan</label>
+                    <input type="text" class="form-control" placeholder="ketik disini..." />
+                </div>
+                
+                
+            </div>
+        </div>
+        
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">v.koordinat (decimal degree)</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-30 capitalize">titik koord. x (titik awal jembatan)</label>
+                    <input type="text" class="form-control" />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">titik koord. y (titik awal jembatan)</label>
+                    <input type="text" class="form-control" />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">titik koord. x (titik akhir jembatan)</label>
+                    <input type="text" class="form-control" />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">titik koord. y (titik akhir jembatan)</label>
+                    <input type="text" class="form-control" />
+                </div>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-6">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">vi.photo dan video</h2>
+            </div>
+            <div class="p-5">
+                <div class="form-inline">
+                    <label class="form-label sm:w-30">photo</label>
+                    <input type="file" class="form-control" />
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">video</label>
+                    <input type="file" class="form-control"/>
+                </div>
+                <div class="form-inline mt-5">
+                    <label class="form-label sm:w-30 capitalize">peta</label>
+                    <input type="file" class="form-control"/>
+                </div>
+            </div>
+        </div>
+        <div class="box col-span-12 lg:col-span-12">
+            <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400 bg-blue-800">
+                <h2 class="text-base text-white  uppercase">vii.Digitasi</h2>
+            </div>
+            <div class="p-5 text-center">
+                <button type="button" class="btn btn-primary w-24" @click="isShowModal=true">
+                    Peta
+                </button>
+            </div>
         </div>
     </div>
+    <div class=" mt-5">
+        <button type="button" class="btn btn-outline-secondary w-24 mr-1">
+            Batal
+        </button>
+        <button type="button" class="btn btn-primary w-24" @click="submit">
+            <!-- <LoadingIcon icon="tail-spin" class="w-8 h-8" v-if="loading" /> -->
+            Simpan
+        </button>
+    </div>
+    <ModalMarker :isShowModal="isShowModal" @submit="getLatLngs" />
+
 </template>
 <script setup>
-    import { ref } from "vue";
-    defineProps({
-        loading: {
-            type: Boolean,
-            default: false
-        },
-        category: {
-            type: Object,
-            default: {name: null}
-        }
-    })
-    const emit = defineEmits(['submit'])
-    function submit() {
-        emit('submit');
+import sendRequest from '@libs/http.js'
+import ModalMarker from './../../components/ModalMarker.vue'
+import { ref, onMounted } from "vue";
+const loading = ref(false);
+const emit = defineEmits(['submit', 'fileChange'])
+const isShowModal = ref(false);
+
+const props = defineProps({
+    loading: {
+        type: Boolean,
+        default: false
+    },
+    road: {
+        type: Object,
+        default: {}
     }
+})
+
+function submit() {
+    emit('submit');
+}
+
+// function onFileChange(e) {
+//     emit('fileChange', e);
+// }
+
+function getLatLngs(latlngs) {
+    props.road.latlngs = latlngs;
+    isShowModal.value = false;
+}
+
+
 </script>
