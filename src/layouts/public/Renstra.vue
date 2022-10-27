a323c9a38e4feebf6b827c4bd97ce506859c036b
Luqman Fanani <luqman.fanani@gmail.com>
2022-10-27 17:58:37 +0800

    renstra

--- src/layouts/public/Renstra.vue
+++ src/layouts/public/Renstra.vue
@@ -7,7 +7,7 @@
                 <div class="flex-shrink max-w-full w-full lg:w-2/3 mt-10 overflow-hidden">
                     <div class="w-full py-3">
                         <h2 class="text-gray-800 text-3xl font-bold">
-                            <span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>Produk Hukum
+                            <span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>Rencana Strategis
                         </h2>
                     </div>
                     <div class="flex flex-row flex-wrap -mx-3">
@@ -20,148 +20,36 @@
                                     </div>
                                 </div>
                                 <div class="intro-y box p-5 mt-5">
-                                    <div class="overflow-y-auto rounded-lg border shadow-2xl">
-                                        <table class="table table-hover">
-                                            <thead class="table-dark">
-                                                <tr>
-                                                    <th class="whitespace-nowrap">Nama berkas</th>
-                                                    <th class="whitespace-nowrap">Tanggal upload</th>
-                                                    <th class="whitespace-nowrap text-center">Action</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="whitespace-nowrap">Undang-Undang Nomor 3 Tahun 2022 tentang Ibu Kota Negara
-                                                    </td>
-                                                    <td class="whitespace-nowrap">1 jan 2022</td>
-                                                    <td class="whitespace-nowrap">
-                                                        <button class="btn btn-sm btn-primary w-24 m-2">
-                                                            <EyeIcon class="w-4 h-4 mr-2" /> Lihat
-                                                        </button>
-                                                        <button class="btn btn-sm btn-success w-24 text-white">
-                                                            <DownloadIcon class="w-4 h-4 mr-2" /> Unduh
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
+                                    <!-- component -->
+                                    <div class="py-16 bg-white">
+                                        <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                                            <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                                                <div class="md:7/12 lg:w-12/12">
+                                                    <h2 class="text-4xl text-gray-900 font-bold md:text-4xl mb-2">Rencana strategis 2020 - 2022</h2>
+                                                    <div class="border-t border-gray-200 border-opacity-100 mb-2"></div>
+                                                    <p class="mt-6 text-gray-600 text-justify"> Renstra Direktorat Jenderal Sumber Daya Air 2020-2024 disusun dengan memperhatikan tuntutan dan dinamika perkembangan lingkungan strategis yang terjadi begitu cepat, melalui Surat Edaran Direktur Jenderal Sumber Daya Air, berisi target kinerja Direktorat Jenderal Sumber Daya Air 5 (lima) tahun ke depan, yang diwujudkan melalui sasaran strategis, sasaran program, dan sasaran kegiatan di lingkungan Direktorat Jenderal SDA, sebagai penerjemahan pengintegrasian sistem perencanaan, pemrograman dan informasi kinerja sebagaimana diamanatkan pada Redesain Sistem Perencanaan dan Penganggaran (RSPP) Nasional. Pencapaian outcome dan output diharapkan dapat mencapai kinerja yang lebih baik dan memenuhi aspek akuntabilitas berlandaskan kepada sistem akuntansi dan barang milik negara, sistem akuntabilitas kinerja instansi pemerintah serta sistem penganggaraan berbasis kinerja.
+                                                    </p>
+                                                    <div class="border-t border-gray-200 border-opacity-100 mt-2"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="intro-y box col-span-12 lg:col-span-6 mt-5">
+                                <div class="flex items-center px-5 py-5 sm:py-3 border-b border-slate-200/60 dark:border-darkmode-400">
+                                    <h2 class="font-medium text-base mr-auto">Lampiran</h2>
+                                </div>
+                                <div class="p-5">
+                                    <div class="flex items-center mt-5">
+                                        <div class="file">
+                                            <a href="" class="w-12 file__icon file__icon--file">
+                                                <div class="file__icon__file-name text-xs">PDF</div>
+                                            </a>
+                                        </div>
+                                        <div class="ml-4">
+                                            <a class="font-medium" href="">Renstra 2020-2022</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -176,7 +64,45 @@
                                 <table class="table table-hover">
                                     <thead class="table-dark rounded-lg">
                                         <tr>
-                                            <th class="whitespace-nowrap">Kategori produk hukum</th>
+                                            <th class="whitespace-nowrap">RPJMD PROV.SULTRA </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td class="whitespace-nowrap">
+                                                <FileTextIcon class="w-4 h-4 mr-2 inline" />Undang-Undang Nomor 3 Tahun 2022
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="whitespace-nowrap">
+                                                <FileTextIcon class="w-4 h-4 mr-2 inline" />Undang-Undang Nomor 3 Tahun 2022
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="whitespace-nowrap">
+                                                <FileTextIcon class="w-4 h-4 mr-2 inline" />Undang-Undang Nomor 3 Tahun 2022
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="whitespace-nowrap">
+                                                <FileTextIcon class="w-4 h-4 mr-2 inline" />Undang-Undang Nomor 3 Tahun 2022
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td class="whitespace-nowrap">
+                                                <FileTextIcon class="w-4 h-4 mr-2 inline" />Undang-Undang Nomor 3 Tahun 2022
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="mb-6">
+                            <div class="overflow-visible rounded-lg border shadow-2xl">
+                                <table class="table table-hover">
+                                    <thead class="table-dark rounded-lg">
+                                        <tr>
+                                            <th class="whitespace-nowrap">RENSTRA SDABM</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -210,7 +136,6 @@
                             </div>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
