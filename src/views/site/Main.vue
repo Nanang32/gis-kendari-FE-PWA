14fd524140c2330804dbddf8cb5cb1f60a8ebd8f
Luqman Fanani <luqman.fanani@gmail.com>
2022-10-31 19:51:51 +0800

    Hapus kontent, katanya terlalu banyak

--- src/views/site/Main.vue
+++ src/views/site/Main.vue
@@ -4,16 +4,7 @@
     <TinySlider />
     <main id="content">
         <!-- advertisement -->
-        <div class="bg-gray-50 py-4 hidden">
-            <div class="xl:container mx-auto px-3 sm:px-4 xl:px-2">
-                <div class="mx-auto table text-center text-sm">
-                    <a class="uppercase" href="#">Advertisement</a>
-                    <a href="#">
-                        <img src="@/assets/img/ads/ads_728.jpg" alt="advertisement area">
-                    </a>
-                </div>
-            </div>
-        </div>
+       
         <!-- hero big grid -->
         <div class="bg-white py-6">
             <div class="xl:container mx-auto px-3 sm:px-4 xl:px-2">
@@ -114,111 +105,6 @@
                 </div>
             </div>
         </div>
-        <!-- block news -->
-        <div class="bg-white">
-            <div class="xl:container mx-auto px-3 sm:px-4 xl:px-2">
-                <div class="flex flex-row flex-wrap">
-                    <!-- Left -->
-                    <div class="flex-shrink max-w-full w-full lg:w-3/3 overflow-hidden">
-                        <div class="w-full py-3">
-                            <h2 class="text-gray-800 text-2xl font-bold">
-                                <span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>Berita Terkini
-                            </h2>
-                        </div>
-                        <div class="flex flex-row flex-wrap -mx-3">
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img6.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img7.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img8.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img9.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img10.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img11.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-        
-        
-        <!-- block news -->
         <div class="bg-gray-50 py-6">
             <div class="xl:container mx-auto px-3 sm:px-4 xl:px-2">
                 <div class="flex flex-row flex-wrap">
@@ -251,90 +137,12 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img27.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img3.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img4.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img5.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img6.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div class="flex flex-row sm:block hover-img">
-                                    <a href="">
-                                        <img class="max-w-full w-full mx-auto" src="@/assets/img/dummy/img7.jpg" alt="alt title">
-                                    </a>
-                                    <div class="py-0 sm:py-3 pl-3 sm:pl-0">
-                                        <h3 class="text-lg font-bold leading-tight mb-2">
-                                            <a href="#">5 Tips to Save Money Booking Your Next Hotel Room</a>
-                                        </h3>
-                                        <p class="hidden md:block text-gray-600 leading-tight mb-1">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                        <a class="text-gray-500" href="#"><span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>Berita Terkini</a>
-                                    </div>
-                                </div>
-                            </div>
+                            
+                            
+                            
+                            
+                            
+                            
                         </div>
                     </div>
                     <!-- right -->
