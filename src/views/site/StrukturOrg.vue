33bab6e351256555a75a50de2d1e28a0a989ad63
Luqman Fanani <luqman.fanani@gmail.com>
2022-10-31 10:31:59 +0800

    halaman struktur organisasi di site

--- src/views/site/StrukturOrg.vue
+++ src/views/site/StrukturOrg.vue
@@ -8,20 +8,19 @@
                     <div class="flex flex-row flex-wrap">
                         <!-- Left -->
                         <div class="flex-shrink max-w-full w-full lg:w-3/3 overflow-hidden">
-                            
                             <div class="flex flex-row flex-wrap -mx-3">
                                 <div class="flex-shrink max-w-full w-full px-3">
                                     <div class="p-4 border border-gray-100 bg-white mb-4">
                                         <div class="flex flex-row items-center">
                                             <div class="rounded-md object-cover overflow-hidden">
-                                                <a href="#"><img class="border max-w-full sm:w-full" src="@/assets/images/tugasfungsi.png" alt="author"></a>
+                                                <a href="#"><img class="border max-w-full sm:w-full" src="@/assets/images/strukturORG.png" alt="author"></a>
                                             </div>
+                                        </div>
+                                        <div class="rounded-md object-cover overflow-hidden transition duration-200 ease-out transform hover:scale-110 shadow-2xl mt-5">
+                                            <a href="#"><img class="border max-w-full sm:w-full" :src="image" alt="author"></a>
                                         </div>
                                     </div>
                                 </div>
-                                <!-- component -->
-                                
-                                
                             </div>
                         </div>
                         <!-- right -->
@@ -35,7 +34,21 @@
 <script setup>
 import Navbar from "../../components/navbar-menu/Main.vue";
 import Footer from "../../components/footer-public/Main.vue";
+import sendRequest from '@libs/http.js'
+import { ref, onMounted } from "vue";
+const loading = ref(false);
+const image = ref('');
+
+onMounted(async () => {
+    const response = await sendRequest({
+        method: 'get',
+        url: '/public/departmentOrganizations',
+    });
+    if ((response !== null) && (response.status === true)) {
+        image.value = response.data.departmentOrganization.file_url
+    }
+});
 </script>
 <style scoped>
-    @import "./style.css";
+@import "./style.css";
 </style>
