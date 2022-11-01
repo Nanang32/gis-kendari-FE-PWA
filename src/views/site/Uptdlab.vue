622536c3ab297e5128a3dc6d853931b2fa811f8f
Handy Handoko <468608+handyhandoko@users.noreply.github.com>
2022-11-01 12:13:29 +0800

    load uptd lab to public

--- src/views/site/Uptdlab.vue
+++ src/views/site/Uptdlab.vue
@@ -51,16 +51,11 @@
                                     <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                                         <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                                             <div class="md:7/12 lg:w-6/12">
-                                                <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>UPTD Laboratoriums</h2>
-                                                <p class="mt-6 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span> {{ uptd.title }}</h2>
+                                                <div class="mt-6 text-gray-600" v-html="uptd.content"></div>
                                             </div>
-                                            <div class="md:5/12 lg:w-5/12">
-                                                <img src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png" alt="image" loading="lazy" width="" height="">
+                                            <div class="md:5/12 lg:w-5/12" v-if="uptd.image_url !== ''">
+                                                <img :src="uptd.image_url" alt="image" loading="lazy" width="" height="">
                                             </div>
                                         </div>
                                     </div>
@@ -83,17 +78,12 @@
                                             <TabPanel class="leading-relaxed">
                                                 <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                                                     <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                                                        <div class="md:5/12 lg:w-5/12">
-                                                            <img src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png" alt="image" loading="lazy" width="" height="">
+                                                        <div class="md:5/12 lg:w-5/12" v-if="aspal.image_url !== ''">
+                                                            <img :src="aspal.image_url" alt="image" loading="lazy" width="" height="">
                                                         </div>
                                                         <div class="md:7/12 lg:w-6/12">
-                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>UPTD Laboratoriums</h2>
-                                                            <p class="mt-6 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>{{ aspal.title }}</h2>
+                                                            <div class="mt-6 text-gray-600" v-html="aspal.content"></div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -101,17 +91,12 @@
                                             <TabPanel class="leading-relaxed">
                                                 <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                                                     <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                                                        <div class="md:5/12 lg:w-5/12">
-                                                            <img src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png" alt="image" loading="lazy" width="" height="">
+                                                        <div class="md:5/12 lg:w-5/12" v-if="beton.image_url !== ''">
+                                                            <img :src="beton.image_url" alt="image" loading="lazy" width="" height="">
                                                         </div>
                                                         <div class="md:7/12 lg:w-6/12">
-                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>UPTD Laboratoriums</h2>
-                                                            <p class="mt-6 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>{{ beton.title }}</h2>
+                                                            <div class="mt-6 text-gray-600" v-html="beton.content"></div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -119,17 +104,12 @@
                                             <TabPanel class="leading-relaxed">
                                                 <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                                                     <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                                                        <div class="md:5/12 lg:w-5/12">
-                                                            <img src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png" alt="image" loading="lazy" width="" height="">
+                                                        <div class="md:5/12 lg:w-5/12" v-if="agregat.image_url !== ''">
+                                                            <img :src="agregat.image_url" alt="image" loading="lazy" width="" height="">
                                                         </div>
                                                         <div class="md:7/12 lg:w-6/12">
-                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>UPTD Laboratoriums</h2>
-                                                            <p class="mt-6 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>{{ agregat.title }}</h2>
+                                                            <div class="mt-6 text-gray-600" v-html="agregat.content"></div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -137,17 +117,12 @@
                                             <TabPanel class="leading-relaxed">
                                                 <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                                                     <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                                                        <div class="md:5/12 lg:w-5/12">
-                                                            <img src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png" alt="image" loading="lazy" width="" height="">
+                                                        <div class="md:5/12 lg:w-5/12" v-if="tanah.image_url !== ''">
+                                                            <img :src="tanah.image_url" alt="image" loading="lazy" width="" height="">
                                                         </div>
                                                         <div class="md:7/12 lg:w-6/12">
-                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>UPTD Laboratoriums</h2>
-                                                            <p class="mt-6 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>{{ tanah.title }}</h2>
+                                                            <div class="mt-6 text-gray-600" v-html="tanah.content"></div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -155,17 +130,12 @@
                                             <TabPanel class="leading-relaxed">
                                                 <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                                                     <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                                                        <div class="md:5/12 lg:w-5/12">
-                                                            <img src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png" alt="image" loading="lazy" width="" height="">
+                                                        <div class="md:5/12 lg:w-5/12" v-if="penyewaan.image_url !== ''">
+                                                            <img :src="penyewaan.image_url" alt="image" loading="lazy" width="" height="">
                                                         </div>
                                                         <div class="md:7/12 lg:w-6/12">
-                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>UPTD Laboratoriums</h2>
-                                                            <p class="mt-6 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"><span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>{{ penyewaan.title }}</h2>
+                                                            <div class="mt-6 text-gray-600" v-html="penyewaan.content"></div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -175,7 +145,8 @@
                                 </div>
                                 <div class="w-full   bg-green-600 shadow-xl shadow-green-200 py-10 px-20 flex justify-between items-center">
                                     <p class=" text-white capitalize"> <span class="text-4xl font-medium">info</span> <br> <span class="text-lg ">Tarif pengujian di UPTD laborotorium SDABM</span></p>
-                                    <button class="px-5 py-3  font-medium text-slate-700 shadow-xl  hover:bg-white duration-150  bg-yellow-400">CEK DISINI </button>
+                                    <a class="px-5 py-3  font-medium text-slate-700 shadow-xl  hover:bg-white duration-150  bg-yellow-400"
+                                    :href="uptd.attachment_url">CEK DISINI </a>
                                 </div>
                             </div>
                         </div>
@@ -188,10 +159,42 @@
     </div>
 </template>
 <script setup>
-import TinySlider from '@/components/tiny-slider/Main.vue';
-    
-import Navbar from "../../components/navbar-menu/Main.vue";
-import Footer from "../../components/footer-public/Main.vue";
+    import TinySlider from '@/components/tiny-slider/Main.vue';
+    import Navbar from "../../components/navbar-menu/Main.vue";
+    import Footer from "../../components/footer-public/Main.vue";
+    import sendRequest from '@libs/http.js'
+    import { ref, onMounted } from "vue";
+
+    const uptdLabs = ref([]);
+    const uptd = ref({});
+    const aspal = ref({});
+    const beton = ref({});
+    const agregat = ref({});
+    const tanah = ref({});
+    const penyewaan = ref({});
+    const KEY_UPTD = 'UPTD';
+    const KEY_ASPAL = 'ASPAL';
+    const KEY_BETON = 'BETON';
+    const KEY_AGREGAT = 'AGREGAT';
+    const KEY_TANAH = 'TANAH';
+    const KEY_PENYEWAAN = 'PENYEWAAN';
+
+
+    onMounted(async () => {
+        const response = await sendRequest({
+            method: 'get',
+            url: '/uptdLabs',
+        });
+        if ((response !== null) && (response.status === true)) {
+            uptdLabs.value = response.data.uptdlabs
+        }
+        uptd.value = uptdLabs.value.find(item => item.key === KEY_UPTD)
+        aspal.value = uptdLabs.value.find(item => item.key === KEY_ASPAL)
+        beton.value = uptdLabs.value.find(item => item.key === KEY_BETON)
+        agregat.value = uptdLabs.value.find(item => item.key === KEY_AGREGAT)
+        tanah.value = uptdLabs.value.find(item => item.key === KEY_TANAH)
+        penyewaan.value = uptdLabs.value.find(item => item.key === KEY_PENYEWAAN)
+    });
 </script>
 <style scoped>
     @import "./style.css";
