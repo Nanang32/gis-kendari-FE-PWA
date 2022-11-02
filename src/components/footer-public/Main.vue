933382884bd4631d29428b0f0694495a069b6e61
Luqman Fanani <luqman.fanani@gmail.com>
2022-11-03 00:08:12 +0800

    copyright kasi hitam putih

--- src/components/footer-public/Main.vue
+++ src/components/footer-public/Main.vue
@@ -71,11 +71,11 @@
             </div>
         </div>
         <!--Start footer copyright-->
-        <div class="footer-dark bg-[#ce7e00] border-t border-gray-200 border-opacity-100">
-            <div class="container py-4 ">
+        <div class="footer-dark bg-black border-t border-gray-200 border-opacity-100">
+            <div class="container py-4">
                 <div class="row">
                     <div class="col-12 col-md text-center">
-                        <p class="d-block my-3 text-black">Copyright © SISDJATAN (sistem informasi sumber daya air jalan jembatan)</p>
+                        <p class="d-block my-3 text-white">Copyright © SISDJATAN (sistem informasi sumber daya air jalan jembatan)</p>
                     </div>
                 </div>
             </div>
